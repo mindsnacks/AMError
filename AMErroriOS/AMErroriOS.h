84120e7605cf4d0a1cff747cd96f03fb20294601
Jacob Eiting <jacob.eiting@gmail.com>
2016-09-30 15:39:33 -0700

    Create a framework.

---
+++ AMErroriOS/AMErroriOS.h
@@ -0,0 +1,18 @@
+//
+//  AMErroriOS.h
+//  AMErroriOS
+//
+//  Created by Jacob Eiting on 9/30/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMErroriOS.
+FOUNDATION_EXPORT double AMErroriOSVersionNumber;
+
+//! Project version string for AMErroriOS.
+FOUNDATION_EXPORT const unsigned char AMErroriOSVersionString[];
+
+
+#import <AMErroriOS/AMError.h>
